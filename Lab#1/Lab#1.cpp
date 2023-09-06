a1df7f0aa675c9b674d6b18e222901db4afad303
mank000 <kozmin_arti@mail.ru>
2023-09-06 20:55:05 +0700

    Добавьте файлы проекта.

---
+++ Lab#1/Lab#1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <clocale>
+using namespace std;
+/**
+    * @brief Лр/ 1 Знакомство со средой разработки
+    * Исходный код на C++
+    * @return Всегда 0
+*/
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    cout << "Hello World! Привет мир!" << endl
+    int var = 42;
+    int unusing;
+
+}
+
