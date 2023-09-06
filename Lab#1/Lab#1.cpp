63cb0f88d612751405a322375c237b4c3b8695b2
mank000 <kozmin_arti@mail.ru>
2023-09-06 20:58:51 +0700

    исправил ошибку в коде.

--- Lab#1/Lab#1.cpp
+++ Lab#1/Lab#1.cpp
@@ -4,14 +4,11 @@ using namespace std;
 /**
     * @brief Лр/ 1 Знакомство со средой разработки
     * Исходный код на C++
-    * @return Всегда 0
+    * @return Всегда 0a
 */
 int main()
 {
     setlocale(LC_ALL, "ru");
-    cout << "Hello World! Привет мир!" << endl
-    int var = 42;
-    int unusing;
-
+    cout << "Hello World! Привет мир!" << endl;
 }
 
